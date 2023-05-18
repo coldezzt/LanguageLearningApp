ca7d28ba7e7641b931866a5aa287554281e5caf5
Ruzan Valeev <ruzan.valeeff@yandex.ru>
2023-05-18 13:52:05 +0700

    Сверстал fragment_dictionary

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavHost
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomNavigationMenu)
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHost
+
+        bottomNavigation.setupWithNavController(navHost.navController)
     }
 }
